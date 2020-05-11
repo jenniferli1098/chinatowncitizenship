c5fa198af44af0e2b18738be69260fec303e22f5
amazhangphyl <phylliszhang@college.harvard.edu>
2020-05-11 03:09:20 -0500

    added cards, head, and foot functions

--- USHistory_Modules/ushistory_module5.php
+++ USHistory_Modules/ushistory_module5.php
@@ -1,132 +1,19 @@
 <?php
+    include 'flashcards.php';
+    include 'header.php';
     include_once '../access.php';
 ?>
 <!DOCTYPE html>
 
-<head>
-
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>US History Module 5</title>
-
-  <!-- Bootstrap core CSS -->
-  <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Custom styles for this template -->
-  <link href="../css/custom.css" rel="stylesheet">
-
-  <!-- Flashcard JS -->
-  <script src="../source.js"></script>
-
-
-</head>
-
-<body id="page-top">
-
-  <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-    <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="main.html">Chinatown Citizenship</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="../english.html">English</a>
-          </li>
-          <li class="nav-item active">
-            <a class="nav-link active" href="../ushistory.html">US History</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="../n400.html">N400</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="../other.html">Other Resources</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="../contact.html">Contact Us</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+  <?php echo head(5)?>
 
     <header class="bg-primary text-white">
         <div class="container text-center">
             <h1>People</h1>
         </div>
     </header>
-    <section id="flashcard">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8 mx-auto">
-          <h3>Flashcards</h3>
-          <div id="flashCardControls" class="carousel slide">
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <div class="flip-card d-block w-100">
-                  <div class="flip-card-inner">
-                    <div class="flip-card-front">
-                      <h1 class="question" style="padding: 100px;">Flashcards Instructions: <br /> Mouse over this card to flip.</h1>
-                    </div>
-                    <div class="flip-card-back">
-                      <p class="answer" style="padding: 100px;">Use the left and right arrows to switch to another card.</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            <?php
-              $sql = "SELECT * FROM USHistory WHERE Unit = 5";
-              $result = mysqli_query($conn, $sql);
-              $datas = array();
-              if (mysqli_num_rows($result) > 0){
-                  while($row = mysqli_fetch_assoc($result)){
-                      $datas[] = $row;
-                  }
-              }
-              $i = 0;
-              foreach ($datas as $data){ 
-                  $answers = array('C2', 'C3');
-                  $ans = $data['C1'];
-                  foreach ($answers as $answer) { ?>
-                      <?php if ($data[$answer] != "") { 
-                        $ans = $ans . ", " . $data[$answer]; }; 
-                  }; ?>
-                <div class="carousel-item">
-                  <div class="flip-card d-block w-100">
-                    <div class="flip-card-inner">
-                      <div class="flip-card-front">
-                        <h1 class="question" style="padding: 100px;"><?php echo $data['Question']; ?></h1>
-                      </div>
-                      <div class="flip-card-back">
-                        <p class="answer" style="padding: 120px;"><?php echo $ans ?></p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-            <?php $i+=1; }; ?>
-              </div>
-            </div>
+    <?php echo cards(5, $conn)?>
 
-            <a class="carousel-control-prev" href="#flashCardControls" role="button" data-slide="prev"
-              data-interval="false">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#flashCardControls" role="button" data-slide="next"
-              data-interval="false">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
     <section id="Review" class="bg-light">
         <div class="container">
             <div class="row">
@@ -168,27 +55,5 @@
         </div>
     </section>
 
-  <!-- Footer -->
-  <footer class="py-5 bg-dark">
-    <div class="container">
-      <p class="m-0 text-center text-white">Chinatown Citizenship 2019</p>
-    </div>
-    <!-- /.container -->
-  </footer>
-
-  <!-- Bootstrap core JavaScript -->
-  <script src="../vendor/jquery/jquery.min.js"></script>
-  <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Plugin JavaScript -->
-  <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
-
-  <script>
-    $(document).ready(function () {
-      $('[data-toggle="tooltip"]').tooltip();
-    });
-  </script>
-
-</body>
-
+    <?php echo foot()?>
 </html>
