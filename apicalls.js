8bfe0fcbd1cfd63c8d8c910f0f88b00bf0c298c5
jenniferli1098 <jenniferli1098@gmail.com>
2020-05-14 21:25:34 -0400

    Big oops lol, the nav bar is back to normal

--- apicalls.js
+++ apicalls.js
@@ -1,6 +1,7 @@
+let apiKey = "AIzaSyD16owbIkDlL8yQ-yisKK8QEH25u6xNjJI"; //how do we hide the key?
+    
 function fetchChinese(idchange, str) {
     let baseUrl = "https://translation.googleapis.com/language/translate/v2?key=";
-    let apiKey = "AIzaSyD16owbIkDlL8yQ-yisKK8QEH25u6xNjJI"; //how do we hide the key?
     let params = "&q="+str+"&target=zh";
 
     var xhttp = new XMLHttpRequest();
@@ -16,3 +17,59 @@ function fetchChinese(idchange, str) {
     xhttp.open("GET", url, true);
     xhttp.send();
 }
+
+function fetchSpeech(str) {
+    const request = {
+
+        input: {text: str},
+    
+        voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
+    
+        audioConfig: {audioEncoding: 'MP3'},
+    
+    };
+
+    var xhttp = new XMLHttpRequest();
+    let url = "https://texttospeech.googleapis.com/v1/text:synthesize/key="+apiKey;
+    xhr.open("POST", url, true);
+    xhttp.send(request);
+    
+}
+
+fetchSpeech("hello this is chinatown citizenship");
+/*
+
+const projectId = 'articulate-case-277023';
+
+const {Translate} = require('@google-cloud/translate').v2;
+const textToSpeech = require('@google-cloud/text-to-speech');
+const fs = require('fs');
+const util = require('util');
+
+const translate = new Translate({projectId});
+const client = new textToSpeech.TextToSpeechClient();
+
+async function speech(str) {
+
+  const request = {
+
+    input: {text: str},
+
+    voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
+
+    audioConfig: {audioEncoding: 'MP3'},
+
+  };
+
+  const [response] = await client.synthesizeSpeech(request);
+
+  const writeFile = util.promisify(fs.writeFile);
+
+  await writeFile('output.mp3', response.audioContent, 'binary');
+
+  console.log('Audio content written to file: output.mp3');
+
+}
+
+speech("I am hungry. Chicken is good!");
+*/
