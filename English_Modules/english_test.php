d08924800d0950f9be4a600a5664fe795f531c4e
amazhangphyl <phylliszhang@college.harvard.edu>
2020-08-15 23:48:22 -0500

    php header for englishtest.php

--- English_Modules/english_test.php
+++ English_Modules/english_test.php
@@ -1,55 +1,12 @@
 <?php
+    include 'header.php';
     include_once 'access.php';
 ?>
 <!DOCTYPE html>
-<?php header('Content-Type: text/html; charset=utf-8'); ?>
+<html lang="en">
 
-<head>
+<?php echo head("Test")?>
 
-  <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>English</title>
-
-  <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Custom styles for this template -->
-  <link href="css/custom.css" rel="stylesheet">
-
-</head>
-<body>
-
-  <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-    <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="main.html">Chinatown Citizenship</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link active" href="english.html">English</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="ushistory.html">US History</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="n400.html">N400</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="other.html">Other Resources</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="contact.html">Contact Us</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
   <header class="bg-primary text-white">
     <div class="container text-center">
       <h1>English Test</h1>
